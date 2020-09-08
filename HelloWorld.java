3e1ea8622c915b381770109ef6d750057a9fa952
Shivam <gmail@gmail.com>
2020-09-08 11:29:44 +0000

    5 time hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
-    public static void main(String args[]) {
-	System.out.println("hey! brother");
-	System.out.println("Yes i did it");        
 
-	System.out.println("Hello World!");
-	System.out.println("This is an exercise");
-    }
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
